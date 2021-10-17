c3c0413fe810a8c207945a2620106f4dc6738d64
kojihun <38236367+kojihun@users.noreply.github.com>
2021-10-17 15:34:43 +0900

    2021-10-17
    - initial commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
+// 파일명: main.js
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// ROUTER
+import router from './routes';
+
+// APP
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
