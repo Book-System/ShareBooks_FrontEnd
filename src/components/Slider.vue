938ac0270740ce56f09a4fad0441d5be2f37a5be
boolce1249 <75169386+boolce1249@users.noreply.github.com>
2021-11-07 22:41:00 +0900

    20211107

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,69 +1,73 @@
 <template>
     <div id="banner" class="col bg-dark p-5 rounded">
+      <br/>
           <h3 class="mypage">이달의 대여</h3><br/>
     </div><br/>
-<section class="pb-7 pt-5" id="testimonial">
-        <div class="container-xxl">
-          <div class="row flex-center h-100">
-            <div class="col-12">
-              <div class="carousel slide" id="carouselTestimonials" data-bs-ride="carousel">
-                <div class="carousel-inner ps-sm-4 ps-md-4 pb-5 pb-md-2 pb-lg-0">
-                  <div class="carousel-item active" data-bs-interval="10000">
-                    <div class="row h-100 align-items-center justify-content-around">
-                      <div class="col-md-4 col-lg-5 col-xl-4 text-center order-1 d-none d-md-block">
-                        <div class="wrapper square shadow-square-top">
-                          <div class="h-100"><img class="rounded-3 mt-4" src="assets/img/gallery/testimonial-image-1.png" alt="testimonials" width="325" /></div>
+
+    <section class="pb-7 pt-5" id="testimonial">
+          <div class="container-xxl">
+            <div class="row flex-center h-100">
+              <div class="col-12">
+                <div class="carousel slide" id="carouselTestimonials" data-bs-ride="carousel">
+                  <div class="carousel-inner ps-sm-4 ps-md-4 pb-5 pb-md-2 pb-lg-0">
+
+                    <div class="carousel-item active" data-bs-interval="3000">
+                      <div class="row h-100 align-items-center justify-content-around">
+                        <div class="col-md-4 col-lg-5 col-xl-4 text-center order-1 d-none d-md-block">
+                          <div class="wrapper square shadow-square-top">
+                            <div class="h-100"><img class="rounded-3 mt-4" src="https://image.aladin.co.kr/product/27659/93/cover500/k752733137_1.jpg" alt="testimonials" width="300" /></div>
+                          </div>
                         </div>
-                      </div>
-                      <div class="col-md-4 col-lg-5 col-xl-6 d-block d-md-none order-1"><img class="rounded-pill" src="assets/img/gallery/testimonial-image-1.png" alt="testimonials" width="100" /></div>
-                      <div class="col-md-6 col-lg-6 col-xl-5 mb-4"><img class="img-fluid" src="assets/img/gallery/quotation.png" alt="testimonials-bg" />
-                        <h2 class="my-5">When it comes to both reliability and quality, <span class="text-primary">ClickR</span> is undoubtly one of best services out there. Team was fast and responsible. 5 stars for them!</h2>
-                        <h6 class="text-700 fs-lg-0 mb-0">Andrinna Malin</h6>
-                        <p class="fw-normal text-700 fs--1">Designer, co-Founder at Nirvana Tech</p>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="carousel-item" data-bs-interval="5000">
-                    <div class="row h-100 align-items-center justify-content-around">
-                      <div class="col-md-4 col-lg-5 col-xl-4 text-center order-1 d-none d-md-block">
-                        <div class="wrapper square shadow-square-top">
-                          <div class="h-100"><img class="rounded-3 mt-4" src="assets/img/gallery/testimonial-image-2.png" alt="testimonials" width="325" /></div>
+
+                        <div class="col-md-6 col-lg-6 col-xl-5 mb-4">
+                          <h4 class="my-5">기록은 기록을 넘어설 수 있을까? 업무시간의 한 줄이, 동료나 친구와 나눈 대화가, 일하는 순간의 감정이, 여행지에서 써내려간 기록이 생각의 도구가 될 수 있을까? 가치 있는 경험이 될 수 있을까? 《기록의 쓸모》는 평범한 직장인이자 마케터인 저자가 수년간 해온 일의 고민과 일상의 영감을 담은, 실용적인 기록물이자 기록의 과정이다.</h4>
+                          <h5 class="text-700 fs-lg-0 mb-0">기록의 쓸모</h5>
+                          <p class="fw-normal text-700 fs--1">이승희</p>
                         </div>
-                      </div>
-                      <div class="col-md-4 col-lg-5 col-xl-6 d-block d-md-none order-1"><img class="rounded-pill" src="assets/img/gallery/testimonial-image-2.png" alt="testimonials" width="100" /></div>
-                      <div class="col-md-6 col-lg-6 col-xl-5 mb-4"><img class="img-fluid" src="assets/img/gallery/quotation.png" alt="testimonials-bg" />
-                        <h2 class="my-5">When it comes to both reliability and quality, <span class="text-primary">ClickR</span> is undoubtly one of best services out there. Team was fast and responsible. 5 stars for them!</h2>
-                        <h6 class="text-700 fs-lg-0 mb-0">Ainara Vergara,</h6>
-                        <p class="fw-normal text-700 fs--1">Designer</p>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="carousel-item" data-bs-interval="3000">
-                    <div class="row h-100 align-items-center justify-content-around">
-                      <div class="col-md-4 col-lg-5 col-xl-4 text-center order-1 d-none d-md-block">
-                        <div class="wrapper square shadow-square-top">
-                          <div class="h-100"><img class="rounded-3 mt-4" src="assets/img/gallery/testimonial-image-3.png" alt="testimonials" width="325" /></div>
-                        </div>
-                      </div>
-                      <div class="col-md-4 col-lg-5 col-xl-6 d-block d-md-none order-1"><img class="rounded-pill" src="assets/img/gallery/testimonial-image-3.png" alt="testimonials" width="100" /></div>
-                      <div class="col-md-6 col-lg-6 col-xl-5 mb-4"><img class="img-fluid" src="assets/img/gallery/quotation.png" alt="testimonials-bg" />
-                        <h2 class="my-5">When it comes to both reliability and quality, <span class="text-primary">ClickR</span> is undoubtly one of best services out there. Team was fast and responsible. 5 stars for them!</h2>
-                        <h6 class="text-700 fs-lg-0 mb-0">Niek Bove,</h6>
-                        <p class="fw-normal text-700 fs--1">Senior Developer</p>
                       </div>
                     </div>
 
+                    <div class="carousel-item" data-bs-interval="3000">
+                      <div class="row h-100 align-items-center justify-content-around">
+                        <div class="col-md-4 col-lg-5 col-xl-4 text-center order-1 d-none d-md-block">
+                          <div class="wrapper square shadow-square-top">
+                            <div class="h-100"><img class="rounded-3 mt-4" src="https://image.aladin.co.kr/product/3225/0/cover500/s212531040_1.jpg" alt="testimonials" width="325" /></div>
+                          </div>
+                        </div>
+
+                        <div class="col-md-6 col-lg-6 col-xl-5 mb-4">
+                          <h4 class="my-5">재난으로 인해 폐허가 된 지역을 관광하는 '재난 여행' 상품만을 판매하는 여행사 '정글'의 10년차 수석 프로그래머인 주인공 '고요나'. 직장에서 밀려날 위기에 처한 그녀가 이번에 향한 곳은 사막의 싱크홀 '무이'다. 요나는 뜻하지 않게 여행지에서 고립되며 엄청난 프로젝트에 휘말리게 된다.</h4>
+                          <h5 class="text-700 fs-lg-0 mb-0">밤의 여행자들</h5>
+                          <p class="fw-normal text-700 fs--1">윤고은</p>
+                        </div>
+                      </div>
+                    </div>
                     
+                    <div class="carousel-item" data-bs-interval="3000">
+                      <div class="row h-100 align-items-center justify-content-around">
+                        <div class="col-md-4 col-lg-5 col-xl-4 text-center order-1 d-none d-md-block">
+                          <div class="wrapper square shadow-square-top">
+                            <div class="h-100"><img class="rounded-3 mt-4" src="https://image.aladin.co.kr/product/28218/87/cover500/8936438603_1.jpg" alt="testimonials" width="325" /></div>
+                          </div>
+                        </div>
+                        
+                        <div class="col-md-6 col-lg-6 col-xl-5 mb-4">
+                          <h4 class="my-5">"팔 년 전 한적한 공원 벤치에 앉아 목 놓아 울다 문득 나무와 들풀이 듣고 있을지도 모른다고 생각했다."(작가의 말) 천선란의 이 소설은 그 순간 시작되었다. 갑자기 식물들의 소리가 들리거나 손톱 사이로 새싹이 자라기 전까지는 자신이 평범한 고등학생이라고 생각하고 살아온 유나인. 그에겐 이제 식물의 목소리가 들린다. 세상 사람은 누구도 목격하지 못한 억울한 죽음을 바라보고 있던 식물들의 선량함이.</h4>
+                          <h4 class="text-700 fs-lg-0 mb-0">나인 (양장)</h4>
+                          <p class="fw-normal text-700 fs--1">천선란</p>
+                        </div>
+                      </div>
+
+                    </div>
                   </div>
-                </div>
-                <div class="row flex-center ps-5 ps-md-6 ps-lg-7">
-                  <div class="col-12 position-relative"><a class="carousel-control-prev carousel-icon z-index-2" href="#carouselTestimonials" style="left:-1.9rem;" role="button" data-bs-slide="prev"><span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="visually-hidden">Previous</span></a><a class="carousel-control-next carousel-icon z-index-2" href="#carouselTestimonials" role="button" data-bs-slide="next" style="right:auto;"><span class="carousel-control-next-icon" aria-hidden="true"></span><span class="visually-hidden">Next</span></a></div>
+                  <div class="row flex-center ps-5 ps-md-6 ps-lg-7">
+                    <div class="col-12 position-relative"><a class="carousel-control-prev carousel-icon z-index-2" href="#carouselTestimonials" style="left:-1.9rem;" role="button" data-bs-slide="prev"><span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="visually-hidden">Previous</span></a><a class="carousel-control-next carousel-icon z-index-2" href="#carouselTestimonials" role="button" data-bs-slide="next" style="right:auto;"><span class="carousel-control-next-icon" aria-hidden="true"></span><span class="visually-hidden">Next</span></a></div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section><br/><br/>
 
 
 </template>
@@ -72,6 +76,10 @@
 
 </script>
 
-<style>
+<style scoped>
+  section{
+    padding-top:80px;
 
+    
+}
 </style>
