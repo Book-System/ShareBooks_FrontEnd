9a66899b9a0a7c6a2484805c94482b60b3fba833
boolce1249 <boolce1249@naver.com>
2021-11-08 09:57:57 +0900

    20211108

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,107 +1,106 @@
 <template>
     <div class="container">
-    <div class="row">
-        <div class="col-md-3">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h3 class="panel-title">고객센터</h3>
-                </div>
+        <div class="row">
+            <div class="col-md-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">고객센터</h3>
+                    </div>
                     <ul class="list-group">
                         <a href="./customer" class="list-group-item">문의하기</a>
                         <a href="./board" class="list-group-item">문의게시판</a>
                         <a href="./Faq" class="list-group-item">자주묻는질문</a>
                     </ul>
                 </div>
-        </div>
+            </div>
 
             <div class="col-md-9">
-            <div class="card" style="padding:30px">
-                <div class="card-body">
-                    <div class="row">
-                        <h4>문의게시판</h4>
-                        <hr/>
-                            
-                        
-                        <section id="article-list-wrap" class="section-1 con-min-width">
-                            <div class="con">
-                                <div class="article-search">
-                                <form class="block">
-                                    <input type="text" @input="search" :value="searchKeyword"
-                                        placeholder="검색어" >
-                                </form>
+                <div class="card" style="padding:30px">
+                    <div class="card-body">
+                        <div class="row">
+                            <h4>문의게시판</h4>
+                            <hr />
 
 
+                            <section id="article-list-wrap" class="section-1 con-min-width">
+                                <div class="con">
+                                    <div class="article-search">
+                                        <form class="block">
+                                            <input type="text" @input="search" :value="searchKeyword" placeholder="검색어">
+                                        </form>
+
+
+                                    </div>
+                                    <table class="table">
+                                        <thead>
+                                            <tr>
+                                                <th scope="col">번호</th>
+                                                <th scope="col">제목</th>
+                                                <th scope="col">작성자</th>
+                                                <th scope="col">날짜</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <th scope="row">1</th>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                            </tr>
+                                            <tr>
+                                                <th scope="row">2</th>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                            </tr>
+                                            <tr>
+                                                <th scope="row">3</th>
+                                                <td colspan="2"></td>
+                                                <td></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
                                 </div>
-                            <table class="table">
-                                <thead>
-                                    <tr>
-                                    <th scope="col">번호</th>
-                                    <th scope="col">제목</th>
-                                    <th scope="col">작성자</th>
-                                    <th scope="col">날짜</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                    <th scope="row">1</th>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    </tr>
-                                    <tr>
-                                    <th scope="row">2</th>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    </tr>
-                                    <tr>
-                                    <th scope="row">3</th>
-                                    <td colspan="2"></td>
-                                    <td></td>
-                                    </tr>
-                                </tbody>
-                                </table>
-                            </div>
-                        </section>
+                            </section>
 
-                        <div style="text-align: center;">
-                        <nav aria-label="Page navigation example">
-                          <ul class="pagination">
-                            <li class="page-item">
-                              <a class="page-link" href="#" aria-label="Previous">
-                                <span aria-hidden="true">&laquo;</span>
-                              </a>
-                            </li>
-                            <li class="page-item"><a class="page-link" href="#">1</a></li>
-                            <li class="page-item"><a class="page-link" href="#">2</a></li>
-                            <li class="page-item"><a class="page-link" href="#">3</a></li>
-                            <li class="page-item">
-                              <a class="page-link" href="#" aria-label="Next">
-                                <span aria-hidden="true">&raquo;</span>
-                              </a>
-                            </li>
-                          </ul>
-                        </nav>
+                            <div style="text-align: center;">
+                                <nav aria-label="Page navigation example">
+                                    <ul class="pagination">
+                                        <li class="page-item">
+                                            <a class="page-link" href="#" aria-label="Previous">
+                                                <span aria-hidden="true">&laquo;</span>
+                                            </a>
+                                        </li>
+                                        <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                        <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                        <li class="page-item">
+                                            <a class="page-link" href="#" aria-label="Next">
+                                                <span aria-hidden="true">&raquo;</span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </nav>
+                            </div>
+
                         </div>
-                        
                     </div>
                 </div>
             </div>
         </div>
     </div>
-  </div>
-    
+
 </template>
 
 <script>
     export default {
-        
+
     }
 </script>
 
 <style scoped>
-    .container{
-    padding-top:20px;
-    padding-bottom:100px;    
-  }
+    .container {
+        padding-top: 20px;
+        padding-bottom: 100px;
+    }
 </style>
--- src/components/Slider2.vue
+++ src/components/Slider2.vue
@@ -1,144 +1,143 @@
 <template>
     <div id="banner" class="col bg-dark p-5 rounded">
-          <h3 class="mypage">이달의 대여</h3><br/>
-          
-    </div><br/>
-  
-<div class="container">
-  
-  <div id="demo" class="carousel slide" data-ride="carousel">
+        <h3 class="mypage">이달의 대여</h3><br />
 
-  <!-- Indicators -->
-  <ul class="carousel-indicators mb-0 pb-0">
-    <li data-target="#demo" data-slide-to="0" class="active"></li>
-    <li data-target="#demo" data-slide-to="1"></li>
-    <li data-target="#demo" data-slide-to="2"></li>
-  </ul>
+    </div><br />
 
-  <!-- The slideshow -->
-  <div class="carousel-inner no-padding my-5">
-    <div class="carousel-item active">
-      <div class="col-xs-4 col-sm-4 col-md-4">
-        <a href="#" onclick=abc(this) class="slider_info">
-          <img class="img-fluid card-img-top" src="http://www.greenart.co.kr/upimage/new_editor/201712/20171213171136.jpg">
-        </a>
-        <p></p>
-      </div>
+    <div class="container">
 
-      <div class="col-xs-4 col-sm-4 col-md-4">
-        <a href="#" onclick=abc(this) class="slider_info">
-          <img class="img-fluid card-img-top rounded-bottom" src="http://www.greenart.co.kr/upimage/new_editor/201712/20171213171136.jpg">
-        </a>
-        <p></p>
-      </div>
+        <div id="demo" class="carousel slide" data-ride="carousel">
 
-      <div class="col-xs-4 col-sm-4 col-md-4">
-        <a href="#" onclick=abc(this) class="slider_info">
-          <img class="img-fluid card-img-top rounded-bottom" src="http://www.greenart.co.kr/upimage/new_editor/201712/20171213171136.jpg">
-        </a>
-        <p> </p>
-      </div>
+            <!-- Indicators -->
+            <ul class="carousel-indicators mb-0 pb-0">
+                <li data-target="#demo" data-slide-to="0" class="active"></li>
+                <li data-target="#demo" data-slide-to="1"></li>
+                <li data-target="#demo" data-slide-to="2"></li>
+            </ul>
 
+            <!-- The slideshow -->
+            <div class="carousel-inner no-padding my-5">
+                <div class="carousel-item active">
+                    <div class="col-xs-4 col-sm-4 col-md-4">
+                        <a href="#" onclick=abc(this) class="slider_info">
+                            <img class="img-fluid card-img-top" src="http://www.greenart.co.kr/upimage/new_editor/201712/20171213171136.jpg">
+                        </a>
+                        <p></p>
+                    </div>
+
+                    <div class="col-xs-4 col-sm-4 col-md-4">
+                        <a href="#" onclick=abc(this) class="slider_info">
+                            <img class="img-fluid card-img-top rounded-bottom" src="http://www.greenart.co.kr/upimage/new_editor/201712/20171213171136.jpg">
+                        </a>
+                        <p></p>
+                    </div>
+
+                    <div class="col-xs-4 col-sm-4 col-md-4">
+                        <a href="#" onclick=abc(this) class="slider_info">
+                            <img class="img-fluid card-img-top rounded-bottom" src="http://www.greenart.co.kr/upimage/new_editor/201712/20171213171136.jpg">
+                        </a>
+                        <p> </p>
+                    </div>
+
+                </div>
+                <div class="carousel-item">
+                    <div class="col-xs-4 col-sm-4 col-md-4">
+                        <a href="#" onclick=abc(this) class="slider_info">
+                            <img class="img-fluid card-img-top" src="http://www.greenart.co.kr/upimage/new_editor/201712/20171213171136.jpg">
+                        </a>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi nesciunt quam obcaecati maiores atque labore fugiat tenetur tempore veritatis temporibus!</p>
+
+                    </div>
+                    <div class="col-xs-4 col-sm-4 col-md-4">
+                        <a href="#" onclick=abc(this) class="slider_info">
+                            <img class="img-fluid card-img-top" src="http://www.greenart.co.kr/upimage/new_editor/201712/20171213171136.jpg">
+                        </a>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi nesciunt quam obcaecati maiores atque labore fugiat tenetur tempore veritatis temporibus!
+                        </p>
+
+                    </div>
+                    <div class="col-xs-4 col-sm-4 col-md-4">
+                        <a href="#" onclick=abc(this) class="slider_info">
+                            <img class="img-fluid card-img-top" src="http://www.greenart.co.kr/upimage/new_editor/201712/20171213171136.jpg">
+                        </a>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi nesciunt quam obcaecati maiores atque labore fugiat tenetur tempore veritatis temporibus!</p>
+
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="col-xs-4 col-sm-4 col-md-4">
+                        <a href="#" onclick=abc(this) class="slider_info">
+                            <img class="img-fluid card-img-top" src="http://www.greenart.co.kr/upimage/new_editor/201712/20171213171136.jpg">
+                        </a>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi nesciunt quam obcaecati maiores atque labore fugiat tenetur tempore veritatis temporibus!</p>
+
+                    </div>
+                    <div class="col-xs-4 col-sm-4 col-md-4">
+                        <a href="#" onclick=abc(this) class="slider_info">
+                            <img class="img-fluid card-img-top" src="http://www.greenart.co.kr/upimage/new_editor/201712/20171213171136.jpg">
+                        </a>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi nesciunt quam obcaecati maiores atque labore fugiat tenetur tempore veritatis temporibus!</p>
+
+                    </div>
+                    <div class="col-xs-4 col-sm-4 col-md-4">
+                        <a href="#" onclick=abc(this) class="slider_info">
+                            <img class="img-fluid card-img-top" src="http://www.greenart.co.kr/upimage/new_editor/201712/20171213171136.jpg">
+                        </a>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi nesciunt quam obcaecati maiores atque labore fugiat tenetur tempore veritatis temporibus!
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+
+            <!-- Left and right controls -->
+            <a class="carousel-control-prev" href="#demo" data-slide="prev">
+                <span class="carousel-control-prev-icon sp"></span>
+            </a>
+            <a class="carousel-control-next" href="#demo" data-slide="next">
+                <span class="carousel-control-next-icon sp"></span>
+            </a>
+        </div>
     </div>
-    <div class="carousel-item">
-      <div class="col-xs-4 col-sm-4 col-md-4">
-        <a href="#" onclick=abc(this) class="slider_info">
-          <img class="img-fluid card-img-top" src="http://www.greenart.co.kr/upimage/new_editor/201712/20171213171136.jpg">
-        </a>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi nesciunt quam obcaecati maiores atque labore fugiat tenetur tempore veritatis temporibus!</p>
-
-      </div>
-      <div class="col-xs-4 col-sm-4 col-md-4">
-        <a href="#" onclick=abc(this) class="slider_info">
-          <img class="img-fluid card-img-top" src="http://www.greenart.co.kr/upimage/new_editor/201712/20171213171136.jpg">
-        </a>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi nesciunt quam obcaecati maiores atque labore fugiat tenetur tempore veritatis temporibus!
-        </p>
-
-      </div>
-      <div class="col-xs-4 col-sm-4 col-md-4">
-        <a href="#" onclick=abc(this) class="slider_info">
-          <img class="img-fluid card-img-top" src="http://www.greenart.co.kr/upimage/new_editor/201712/20171213171136.jpg">
-        </a>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi nesciunt quam obcaecati maiores atque labore fugiat tenetur tempore veritatis temporibus!</p>
-
-      </div>
-    </div>
-    <div class="carousel-item">
-      <div class="col-xs-4 col-sm-4 col-md-4">
-        <a href="#" onclick=abc(this) class="slider_info">
-          <img class="img-fluid card-img-top" src="http://www.greenart.co.kr/upimage/new_editor/201712/20171213171136.jpg">
-        </a>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi nesciunt quam obcaecati maiores atque labore fugiat tenetur tempore veritatis temporibus!</p>
-
-      </div>
-      <div class="col-xs-4 col-sm-4 col-md-4">
-        <a href="#" onclick=abc(this) class="slider_info">
-          <img class="img-fluid card-img-top" src="http://www.greenart.co.kr/upimage/new_editor/201712/20171213171136.jpg">
-        </a>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi nesciunt quam obcaecati maiores atque labore fugiat tenetur tempore veritatis temporibus!</p>
-
-      </div>
-      <div class="col-xs-4 col-sm-4 col-md-4">
-        <a href="#" onclick=abc(this) class="slider_info">
-          <img class="img-fluid card-img-top" src="http://www.greenart.co.kr/upimage/new_editor/201712/20171213171136.jpg">
-        </a>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi nesciunt quam obcaecati maiores atque labore fugiat tenetur tempore veritatis temporibus!
-        </p>
-
-      </div>
-    </div>
-  </div>
-
-  <!-- Left and right controls -->
-  <a class="carousel-control-prev" href="#demo" data-slide="prev">
-     <span class="carousel-control-prev-icon sp"></span>
-                </a>
-  <a class="carousel-control-next" href="#demo" data-slide="next">
-                    <span class="carousel-control-next-icon sp"></span>
-                </a>
-</div>
-</div>
 
 
 
 
 
-    
+
 </template>
 
 <script>
-    
+
 </script>
 
 <style>
+    .col-md-4 {
+        display: inline-block;
+        margin-left: -10px;
+    }
 
-.col-md-4{
-  display: inline-block;
-  margin-left:-10px;
-}
-.col-md-4 img{
-  width:100%;
-  height:auto;
-}
-  
-body .carousel-control-prev-icon,
-body .carousel-indicators li,
-body .carousel-control-next-icon{
-  background-color:#000;
-}
+    .col-md-4 img {
+        width: 100%;
+        height: auto;
+    }
 
-span.carousel-control-prev-icon {
- position: relative;
- bottom: 40px;
- right: 40px;
-}
+    body .carousel-control-prev-icon,
+    body .carousel-indicators li,
+    body .carousel-control-next-icon {
+        background-color: #000;
+    }
 
-span.carousel-control-next-icon {
- position: relative;
- bottom: 40px;
-}
+    span.carousel-control-prev-icon {
+        position: relative;
+        bottom: 40px;
+        right: 40px;
+    }
 
+    span.carousel-control-next-icon {
+        position: relative;
+        bottom: 40px;
+    }
 </style>
