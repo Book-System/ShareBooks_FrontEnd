5f89a88d44a11eb836455222c1c51bf44d6e4e88
hanny310 <85853134+hanny310@users.noreply.github.com>
2021-11-10 01:15:08 +0900

    home한글변경
    home화면 한글 임시변경

--- src/components/Home2.vue
+++ src/components/Home2.vue
@@ -4,14 +4,17 @@
         <div class="container"  >
             <div class="main1-box1"  >
                 <div class="main1-box1-sub">
-                <div class="main1-text1">
-                    Search for Books
-                </div>
                 <div class="main1-text2">
-                     around you
+                    주변에서 빌릴 수 
+                </div>
+                 <div class="main1-text2">
+                     있는 책을
+                </div>
+                <div class="main1-text1">
+                     찾아보세요!
                 </div>
                 <div class="main-text-btn">
-                  <a href="./BookList2" class="myButton">Take a look</a>
+                  <a href="./BookList2" class="myButton">목록보기</a>
                 </div>
                 </div>
             </div>
@@ -28,14 +31,14 @@
             </div>
             <div class="main1-box1"  >
                 <div class="main2-box2-sub">
-                <div class="main2-text1">
-                    Sharing
-                </div>
                 <div class="main1-text2">
-                   Unused Books
+                    안쓰는 책을 
+                </div>
+                <div class="main1-text1">
+                  공유해보세요!
                 </div>
                 <div class="main-text-btn">
-                  <a href="./Register" class="myButton">Register</a>
+                  <a href="./Register" class="myButton">책 등록하기</a>
                 </div>
                 </div>
             </div>
@@ -47,18 +50,18 @@
                 <div class="main1-box1-sub">
                
                 <div class="main1-text2">
-                    Book of the
+                    이번달 제일 많이 빌려간 책
                 </div>
-                 <div class="main1-text1">
-                   Month
+                 <div class="main1-text2">
+                   순위를 확인해보세요
                 </div>
                 <div class="main-text-btn">
-                  <a href="./Slider" class="myButton">Recommend</a>
+                  <a href="./Slider" class="myButton">이달의 책 추천</a>
                 </div>
                 </div>
             </div>
             <div class="main1-box2">
-                <img src="../assets//images/Winners1.png" style="width:300px; height:300px; " />
+                <img src="../assets/images/Winners1.png" style="width:300px; height:300px; " />
             </div>
         </div>
 
@@ -66,18 +69,19 @@
     <div class="main background-color-white">
          <div class="container"  >
             <div class="main2-box1">
-                <img src="../assets//images/chatting2.png" style="width:300px; height:300px; " />
+                <img src="../assets/images/chatting2.png" style="width:300px; height:300px; " />
             </div>
             <div class="main1-box1"  >
                 <div class="main2-box2-sub">
-                <div class="main1-text1">
-                    Reserve
+                <div class="main1-text2">
+                    예약은 손쉽게
                 </div>
                 <div class="main1-text2">
-                     By Chat
+                     채팅으로
                 </div>
+                
                 <div class="main-text-btn">
-                  <a href="./Reserve" class="myButton">Reservation</a>
+                  <a href="./Reserve" class="myButton">예약하기</a>
                 </div>
                 </div>
             </div>
@@ -90,19 +94,19 @@
             <div class="main1-box1"  >
                 <div class="main1-box1-sub">
                 <div class="main1-text2">
-                  Recommendation through
+                 내 취향의 책을
                 </div>
                 <div class="main1-text1">
-                   taste analysis
+                   추천받아보세요
                 </div>
               
                 <div class="main-text-btn">
-                  <a href="./Join" class="myButton">Please Join</a>
+                  <a href="./Join" class="myButton">회원되기</a>
                 </div>
                 </div>
             </div>
             <div class="main1-box2">
-                <img src="../assets//images/analyzing.png" style="width:300px; height:300px; " />
+                <img src="../assets/images/analyzing.png" style="width:300px; height:300px; " />
             </div>
         </div>
 
@@ -155,8 +159,8 @@
      
 }
 .main1-text1{
-    font-size: 55px;
-    font-weight: bold;
+    font-size: 45px;
+    font-weight: normal;
 }
 .main1-text2{
     font-size: 30px;
@@ -169,8 +173,8 @@
 	cursor:pointer;
 	color:#ffffff;
 	font-family:Trebuchet MS;
-	font-size:20px;
-	padding:13px 17px;
+	font-size:18px;
+	padding:10px 17px;
 	text-decoration:none;
 }
 .myButton:hover {
